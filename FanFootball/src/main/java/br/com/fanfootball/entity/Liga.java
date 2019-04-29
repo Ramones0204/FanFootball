39b75fa70f3310661abb2a1dd31526f51e23870e
Ramon <ramonroliveirafilho@gmail.com>
2019-04-28 23:24:06 -0300

    Criado as classes Liga,Marca,Time,TipoProduto, foi adcionado no formulario de time, a lista de estado e região

---
+++ FanFootball/src/main/java/br/com/fanfootball/entity/Liga.java
@@ -0,0 +1,22 @@
+package br.com.fanfootball.entity;
+
+public class Liga {
+	private int id;
+	private String nomeLiga;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNomeLiga() {
+		return nomeLiga;
+	}
+	public void setNomeLiga(String nomeLiga) {
+		this.nomeLiga = nomeLiga;
+	}
+	
+	
+	
+}
---
+++ FanFootball/src/main/java/br/com/fanfootball/entity/TipoProduto.java
@@ -0,0 +1,21 @@
+package br.com.fanfootball.entity;
+
+public class TipoProduto {
+	private int id;
+	private String descricao;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
